067679263cc52ed0fed54e8fe74bcb78a22bfa52
psychoAB <dome.2499@hotmail.com>
2016-08-19 15:16:58 +0700

    add prepare world

--- 01/modern-crab/CrabWorld.java
+++ 01/modern-crab/CrabWorld.java
@@ -16,5 +16,36 @@ public class CrabWorld extends World
     public CrabWorld()
     {    
         super(560, 560, 1); 
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        prepCrab();
+        prepWorm();
+    }
+    
+    public void prepCrab()
+    {
+        Actor crab;
+        crab = new Crab();
+        addObject(crab, 280, 280);
+    }
+    
+    public void prepWorm()
+    {
+        Actor worm;
+        int x, y;
+        for(int i=0; i<15; i++)
+        {
+            worm = new Worm();
+            x = Greenfoot.getRandomNumber(500);
+            y = Greenfoot.getRandomNumber(500);
+            addObject(worm, x, y);
+        }
     }
 }
