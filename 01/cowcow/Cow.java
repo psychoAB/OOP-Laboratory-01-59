1c405d3c71c8ec2c06064eef81d43be43f967b4b
psychoAB <dome.2499@hotmail.com>
2016-08-25 00:32:04 +0700

    change window size, add Cow class, add Player class

---
+++ 01/cowcow/Cow.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Cow here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Cow extends Actor
+{
+    /**
+     * Act - do whatever the Cow wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
