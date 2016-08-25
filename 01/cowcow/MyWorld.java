7c3e21fdd9414beba01a2ec5c363632f5439bbfb
psychoAB <dome.2499@hotmail.com>
2016-08-25 17:38:35 +0700

    player can move between cows

--- 01/cowcow/MyWorld.java
+++ 01/cowcow/MyWorld.java
@@ -17,5 +17,10 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 600, 1); 
+        worldInit();
+    }
+    private void worldInit()
+    {
+        Greenfoot.setSpeed(37);
     }
 }
--- 01/cowcow/Player.java
+++ 01/cowcow/Player.java
@@ -1,4 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * Write a description of class Player here.
@@ -12,8 +14,74 @@ public class Player extends Actor
      * Act - do whatever the Player wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+
+    List<Cow> remainCows;
+
     public void act() 
     {
         // Add your action code here.
-    }    
+        getRemainCow();
+        if(Greenfoot.isKeyDown("up"))
+        {
+            moveUp();
+        }
+        if(Greenfoot.isKeyDown("down"))
+        {
+            moveDown();
+        }
+    }
+    
+    private void getRemainCow()
+    {
+        World world = getWorld();
+        remainCows = world.getObjects(Cow.class);
+    }
+
+    private void moveUp()
+    {
+        List<Cow> upCows = new ArrayList<Cow>();
+        Cow nearestUpCow = null;
+        for(int i = 0; i < remainCows.size(); i++)
+        {
+            if(remainCows.get(i).getY() < this.getY())
+            {
+                upCows.add(remainCows.get(i));
+            }
+        }
+        for(int i = 0; i < upCows.size(); i++)
+        {
+            if(nearestUpCow == null || nearestUpCow.getY() < upCows.get(i).getY())
+            {
+                nearestUpCow = upCows.get(i);
+            }
+        }
+        if(nearestUpCow!= null)
+        {
+            this.setLocation(nearestUpCow.getX(), nearestUpCow.getY());
+        }
+    }
+    
+    private void moveDown()
+    {
+        List<Cow> downCows = new ArrayList<Cow>();
+        Cow nearestDownCow = null;
+        for(int i = 0; i < remainCows.size(); i++)
+        {
+            if(remainCows.get(i).getY() > this.getY())
+            {
+                downCows.add(remainCows.get(i));
+            }
+        }
+        for(int i = 0; i < downCows.size(); i++)
+        {
+            if(nearestDownCow == null || nearestDownCow.getY() > downCows.get(i).getY())
+            {
+                nearestDownCow = downCows.get(i);
+            }
+        }
+        if(nearestDownCow != null)
+        {
+            this.setLocation(nearestDownCow.getX(), nearestDownCow.getY());
+        }
+    }
 }
