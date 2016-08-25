9789f86a6769767a1460812aa844e1818280b3e6
psychoAB <dome.2499@hotmail.com>
2016-08-25 23:19:11 +0700

    add MilkBar class

---
+++ 01/cowcow/MilkBar.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MilkBar here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MilkBar extends Actor
+{
+    /**
+     * Act - do whatever the MilkBar wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
