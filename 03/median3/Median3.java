93cfb7f73fae7714cb0cff0bb0c3e719e20e580b
psychoAB <dome.2499@hotmail.com>
2016-09-09 00:07:35 +0700

    Median3: complete requirement

--- 03/median3/Median3.java
+++ 03/median3/Median3.java
@@ -1,5 +1,9 @@
+import java.util.Arrays;
+
 public class Median3 {
     public static int median(int a, int b, int c) {
-        return 0;
+        int[] data = {a, b, c};
+        Arrays.sort(data);
+        return data[1];
     }
 }
