1d8d0bafdc2a13ceec4de50be8410cb0203ab66a
psychoAB <dome.2499@hotmail.com>
2016-09-08 23:39:50 +0700

    Membership: complete requirement

--- 03/membership/Membership.java
+++ 03/membership/Membership.java
@@ -1,5 +1,21 @@
 public class Membership {
     public static double getDiscount (double price, boolean isMember, int memberPoints) {
+        if (!isMember) {
+            return price;
+        }
+        if (price <= 1000)
+        {
+            price = discount(price, 10);
+        } else {
+            price = discount(price, 5);
+        }
+        if (memberPoints > 500) {
+            price = discount(price, 2.5);
+        }
         return price;
+    }
+
+    public static double discount(double price, double percent) {
+        return price * (100 - percent) / 100;
     }
 }
