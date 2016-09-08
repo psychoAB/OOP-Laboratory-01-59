fcfa17bfee5e8d3167ad7cc1880c98789dfbbeeb
psychoAB <dome.2499@hotmail.com>
2016-09-08 23:06:14 +0700

    init Membership

---
+++ 03/membership/Membership.java
@@ -0,0 +1,5 @@
+public class Membership {
+    public static double getDiscount (double price, boolean isMember, int memberPoints) {
+
+    }
+}
---
+++ 03/membership/MembershipTest.java
@@ -0,0 +1,25 @@
+public class MembershipTest {
+     
+    @Test
+    public void testNonMember() {
+        assertEquals(1000, Membership.getDiscount(1000, false, 0), 0.001);
+        assertEquals(400, Membership.getDiscount(400, false, 0), 0.001);
+    }
+
+    @Test
+    public void testMemberWithSmallPoints() {
+        assertEquals(450, Membership.getDiscount(500, true, 0), 0.001);
+        assertEquals(450, Membership.getDiscount(500, true, 500), 0.001);
+        assertEquals(900, Membership.getDiscount(1000, true, 100), 0.001);
+        assertEquals(950.94999, Membership.getDiscount(1001, true, 100), 0.001);
+        assertEquals(1900, Membership.getDiscount(2000, true, 500), 0.001);
+    }
+
+    @Test
+    public void testMemberWithHighPoints() {
+        assertEquals(438.75, Membership.getDiscount(500, true, 501), 0.001);
+        assertEquals(877.5, Membership.getDiscount(1000, true, 600), 0.001);
+        assertEquals(927.1762, Membership.getDiscount(1001, true, 600), 0.001);
+        assertEquals(1852.5, Membership.getDiscount(2000, true, 501), 0.001);
+    }
+}
