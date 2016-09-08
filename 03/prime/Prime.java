194b6febced731071238105fe59e7bba3a2a695c
psychoAB <dome.2499@hotmail.com>
2016-09-09 00:50:19 +0700

    Prime: complete requirement

--- 03/prime/Prime.java
+++ 03/prime/Prime.java
@@ -1,5 +1,14 @@
 public class Prime {
     public static boolean isPrime(int n) {
-        return false;
+        if (n < 2) {
+            return false;
+        }
+        for(int divider = 2; divider < n; divider++) {
+            if (n != divider && n % divider == 0)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
