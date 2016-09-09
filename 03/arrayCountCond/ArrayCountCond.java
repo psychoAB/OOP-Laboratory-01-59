ece4193a965b861cd3a36c86148a2ef5c60c02ab
psychoAB <dome.2499@hotmail.com>
2016-09-09 14:32:01 +0700

    ArrayCountCond: complete requirement

--- 03/arrayCountCond/ArrayCountCond.java
+++ 03/arrayCountCond/ArrayCountCond.java
@@ -1,5 +1,13 @@
 public class ArrayCountCond {
     public static int countGreaterThan(int[] ar, int x) {
-        return 0;
+        int arrayLenth = ar.length;
+        int count = 0;
+
+        for(int index = 0; index < arrayLenth; index++) {
+            if(ar[index] > x) {
+                count++;
+            }
+        }
+        return count;
     }
 }
