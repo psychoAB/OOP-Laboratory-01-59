3b14afd4018ba626be11cd77795667e0e6f9e5fb
psychoAB <dome.2499@hotmail.com>
2016-09-09 14:10:27 +0700

    ArrayReverse: complete requirement

--- 03/arrayReverse/ArrayReverse.java
+++ 03/arrayReverse/ArrayReverse.java
@@ -1,6 +1,12 @@
 public class ArrayReverse {
     public static int[] reverse(int[] ar) {
         int[] res = new int[ar.length];
+        int arrayLength = ar.length;
+        
+        for (int index = 0; index < arrayLength; index++)
+        {
+            res[index] = ar[arrayLength - index - 1];
+        }
         return res;
     } 
 }
