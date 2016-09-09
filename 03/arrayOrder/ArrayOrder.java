9a9e27074b2412d658778ab76daf1d0c38ff5c70
psychoAB <dome.2499@hotmail.com>
2016-09-09 13:57:41 +0700

    ArrayOrder: complete requirement

--- 03/arrayOrder/ArrayOrder.java
+++ 03/arrayOrder/ArrayOrder.java
@@ -1,5 +1,14 @@
 public class ArrayOrder {
     public static boolean isAscending(int[] ar) {
-        return false;
+        int arrayLength = ar.length;
+        int lastElement = ar[0];
+
+        for(int index = 1; index < arrayLength; index++) {
+            if (lastElement > ar[index]){
+                return false;
+            }
+            lastElement = ar[index];
+        }
+        return true;
     }
 }
