64eafc1faecc4b419e4ca731c1cf87508c0bf313
psychoAB <dome.2499@hotmail.com>
2016-09-09 15:22:41 +0700

    init counter

---
+++ 03/oopIntro/counter/Counter.java
@@ -0,0 +1,11 @@
+public class Counter {
+    int currentCounter = 0;
+    
+    public int getValue() {
+        return currentCounter;
+    }
+    
+    public void increase() {
+        currentCounter++;
+    }
+}
