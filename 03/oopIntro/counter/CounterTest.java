04b9010b15320b1b528c7a23bb4e934777410085
psychoAB <dome.2499@hotmail.com>
2016-09-09 15:33:51 +0700

    Counter: fix syntax error, import junit.Test

--- 03/oopIntro/counter/CounterTest.java
+++ 03/oopIntro/counter/CounterTest.java
@@ -1,18 +1,22 @@
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
 public class CounterTest {
     
-    @test
+    @Test
     public void testShouldBeAbleToCreateCounter() {
         Counter counter = new Counter();
         assertNotEquals(null, counter);
     }
 
+    @Test
     public void testCounterShouldReturnZeroAfterCreated() {
         Counter counter = new Counter();
-        assertEquals(0, counter.getValue())
+        assertEquals(0, counter.getValue());
     }
     
+    @Test
     public void testCounterShouldIncreaseByOne() {
         Counter counter = new Counter();
         counter.increase();
