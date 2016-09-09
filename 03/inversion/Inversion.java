504c4802ea104c454c11f9e4686a9ed450450b99
psychoAB <dome.2499@hotmail.com>
2016-09-09 14:45:34 +0700

    Inversion: complete requirement

--- 03/inversion/Inversion.java
+++ 03/inversion/Inversion.java
@@ -1,5 +1,15 @@
 public class Inversion {
     public static int count(int[] ar) {
-        return 0;
+        int arrayLength = ar.length;
+        int count = 0;
+        
+        for(int index = 0; index < arrayLength; index++) {
+            for(int pairIndex = index; pairIndex < arrayLength; pairIndex++) {
+                if(ar[index] > ar[pairIndex]) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
