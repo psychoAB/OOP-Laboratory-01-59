b2cd06a08c66da2a404e3bdac21e349097dfffd9
psychoAB <dome.2499@hotmail.com>
2016-09-09 14:20:37 +0700

    ArrayFind: complete requirement

--- 03/arrayFind/ArrayFind.java
+++ 03/arrayFind/ArrayFind.java
@@ -1,5 +1,12 @@
 public class ArrayFind {
     public static int find(int[] ar, int x) {
+        int arrayLength = ar.length;
+        
+        for(int index = 0; index < arrayLength; index++) {
+            if(ar[index] == x) {
+                return index;
+            }
+        }
         return -1;
     }
 }
