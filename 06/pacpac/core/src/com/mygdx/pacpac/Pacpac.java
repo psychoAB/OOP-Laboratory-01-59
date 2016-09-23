1d42db94556877b1ffda758f4d06d03bdec6d050
psychoAB <dome.2499@hotmail.com>
2016-09-23 16:02:14 +0700

    init Pacpac

---
+++ 06/pacpac/core/src/com/mygdx/pacpac/Pacpac.java
@@ -0,0 +1,28 @@
+package com.mygdx.pacpac;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Pacpac extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+	}
+
+	@Override
+	public void render () {
+		batch.begin();
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+	}
+}
