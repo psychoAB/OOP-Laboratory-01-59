9c481b58a5d0b717bf3bab255673035b678fbc75
psychoAB <dome.2499@hotmail.com>
2016-10-07 15:21:53 +0700

    Pacpac: render pacman.png at 100,100

--- 06-07/pacpac/core/src/com/mygdx/pacpac/GameScreen.java
+++ 06-07/pacpac/core/src/com/mygdx/pacpac/GameScreen.java
@@ -1,16 +1,23 @@
 package com.mygdx.pacpac;
 
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameScreen extends ScreenAdapter {
     private Pacpac pacpac;
+    private Texture pacmanImg;
     
     public GameScreen(Pacpac pacpac) {
         this.pacpac = pacpac;
+        pacmanImg = new Texture("pacman.png");
     }
 
     @Override
     public void render(float delta) {
-        System.out.println("Hello " + delta);
+        SpriteBatch batch = pacpac.batch;
+        batch.begin();
+        batch.draw(pacmanImg, 100, 100);
+        batch.end();
     }
 }
