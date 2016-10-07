e2be468031c64f2d04498dfe24aa79f75eac673b
psychoAB <dome.2499@hotmail.com>
2016-10-07 14:46:47 +0700

    Pacpac: create GameScreen class

---
+++ 06-07/pacpac/core/src/com/mygdx/pacpac/GameScreen.java
@@ -0,0 +1,11 @@
+package com.mygdx.pacpac;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+public class GameScreen extends ScreenAdapter {
+    private Pacpac pacpac;
+    
+    public GameScreen(Pacpac pacpac) {
+        this.pacpac = pacpac;
+    }
+}
