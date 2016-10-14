8a513f8d0c5ba3bc04b524af9a4599398fcd2165
psychoAB <dome.2499@hotmail.com>
2016-10-14 15:16:54 +0700

    Pacpac: add constructor, getPacman() in World class

--- 06-07/pacpac/core/src/com/mygdx/pacpac/World.java
+++ 06-07/pacpac/core/src/com/mygdx/pacpac/World.java
@@ -1,5 +1,15 @@
 package com.mygdx.pacpac;
 
 public class World {
+    private Pacpac pacpac;
+    private Pacman pacman;
 
+    World(Pacpac pacpac) {
+        this.pacpac = pacpac;
+        pacman = new Pacman(100, 100);
+    }
+
+    Pacman getPacman() {
+        return pacman;
+    }
 }
