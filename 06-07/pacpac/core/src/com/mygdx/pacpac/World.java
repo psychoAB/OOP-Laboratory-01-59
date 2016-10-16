b0500cbc19c15816807b532140cd6dbdbb7c1f2b
psychoAB <dome.2499@hotmail.com>
2016-10-16 13:53:40 +0700

    Pacpac: create maze in Wolrd class

--- 06-07/pacpac/core/src/com/mygdx/pacpac/World.java
+++ 06-07/pacpac/core/src/com/mygdx/pacpac/World.java
@@ -3,13 +3,19 @@ package com.mygdx.pacpac;
 public class World {
     private Pacpac pacpac;
     private Pacman pacman;
+    private Maze maze;
 
     World(Pacpac pacpac) {
         this.pacpac = pacpac;
         pacman = new Pacman(100, 100);
+        maze = new Maze();
     }
 
     Pacman getPacman() {
         return pacman;
+    }
+
+    Maze getMaze() {
+        return maze;
     }
 }
