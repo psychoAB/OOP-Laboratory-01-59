aec4062179ed49dd81bb2b472a91adda52feb465
psychoAB <dome.2499@hotmail.com>
2016-10-16 14:33:14 +0700

    Pacpac: MazeRenderer class can render maze and dot

--- 06-07/pacpac/core/src/com/mygdx/pacpac/MazeRenderer.java
+++ 06-07/pacpac/core/src/com/mygdx/pacpac/MazeRenderer.java
@@ -1,5 +1,38 @@
 package com.mygdx.pacpac;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.Texture;
+
 public class MazeRenderer {
 
+    private SpriteBatch batch;
+    private Maze maze;
+
+    private Texture wallImg;
+    private Texture dotImg;
+
+    public MazeRenderer(SpriteBatch batch, Maze maze) {
+        this.batch = batch;
+        this.maze = maze;
+
+        wallImg = new Texture("wall.png");
+        dotImg = new Texture("dot.png");
+    }
+
+    public void render() {
+        batch.begin();
+        for(int r = 0; r < maze.getHeight(); r++) {
+            for(int c = 0; c < maze.getWidth(); c++) {
+                int x = c * 40;
+                int y = 600 - (r * 40) - 40;
+
+                if(maze.hasWallAt(r, c)) {
+                    batch.draw(wallImg, x, y);
+                } else if(maze.hasDotAt(r, c)) {
+                    batch.draw(dotImg, x, y);
+                }
+            }
+        }
+        batch.end();
+    }
 }
