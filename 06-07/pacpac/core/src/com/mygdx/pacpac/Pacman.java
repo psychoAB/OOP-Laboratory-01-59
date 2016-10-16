f9d26f828314aeae7dc2885d6ef8834bda4d641e
psychoAB <dome.2499@hotmail.com>
2016-10-16 21:36:08 +0700

    Pacpac: pacman can not go through wall

--- 06-07/pacpac/core/src/com/mygdx/pacpac/Pacman.java
+++ 06-07/pacpac/core/src/com/mygdx/pacpac/Pacman.java
@@ -24,15 +24,24 @@ public class Pacman {
     private int currentDirection;
     private int nextDirection;
 
-    public Pacman(int x, int y) {
+    Maze maze;
+
+    public Pacman(int x, int y, Maze maze) {
+        this.maze = maze;
+
         position = new Vector2(x, y);
+
         currentDirection = DIRECTION_STILL;
         nextDirection = DIRECTION_STILL;
     }
 
     public void update() {
         if(isAtCenter()) {
-            currentDirection = nextDirection;
+            if(canMoveInDirection(nextDirection)) {
+                currentDirection = nextDirection;
+            } else {
+                currentDirection = DIRECTION_STILL;
+            }
         }
 
         position.x += SPEED * DIRECTION_OFFSETS[currentDirection][0];
@@ -45,6 +54,13 @@ public class Pacman {
         return ((((int)position.x - blockSize / 2) % blockSize) == 0) &&
                 ((((int)position.y - blockSize / 2) % blockSize) == 0);
     }
+
+    private boolean canMoveInDirection(int direction) {
+        int newRow = getRow() + DIRECTION_OFFSETS[nextDirection][1];
+        int newColumn = getColumn() + DIRECTION_OFFSETS[nextDirection][0];
+
+        return !maze.hasWallAt(newRow, newColumn);
+    }
     
     public void setNextDirection(int direction) {
         nextDirection = direction;
@@ -52,5 +68,13 @@ public class Pacman {
 
     public Vector2 getPosition() {
         return position;
+    }
+
+    private int getRow() {
+        return ((int)position.y) / WorldRenderer.BLOCK_SIZE;
+    }
+
+    private int getColumn() {
+        return ((int)position.x) / WorldRenderer.BLOCK_SIZE;
     }
 }
--- 06-07/pacpac/core/src/com/mygdx/pacpac/World.java
+++ 06-07/pacpac/core/src/com/mygdx/pacpac/World.java
@@ -2,13 +2,14 @@ package com.mygdx.pacpac;
 
 public class World {
     private Pacpac pacpac;
-    private Pacman pacman;
     private Maze maze;
+    private Pacman pacman;
 
     World(Pacpac pacpac) {
         this.pacpac = pacpac;
-        pacman = new Pacman(60, 60);
+
         maze = new Maze();
+        pacman = new Pacman(60, 60, maze);
     }
 
     public void update(float delta) {
