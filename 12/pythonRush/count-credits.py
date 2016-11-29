d5447fb98fedfd84912956a054440f32ad545528
psychoAB <dome.2499@hotmail.com>
2016-11-29 16:36:44 +0700

    init week12 + init pythonRush + add count-credits.py to pythonRush

---
+++ 12/pythonRush/count-credits.py
@@ -0,0 +1,11 @@
+f = open('courses.txt', 'r')
+s = f.read()
+f.close()
+credits = 0
+
+for line in s.split('\n'):
+    if line != '':
+        credit = line.split(',')[1]
+        credits += int(credit)
+
+print(credits)
