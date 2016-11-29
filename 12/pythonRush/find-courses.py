dea3ec3a22c03ede6559951e7a0bf1c7f5fed092
psychoAB <dome.2499@hotmail.com>
2016-11-29 16:53:56 +0700

    week12: complete find-courses.py

--- 12/pythonRush/find-courses.py
+++ 12/pythonRush/find-courses.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+import sys
+
+f = open('courses.txt', 'r')
+code = sys.argv[1]
+s = f.read()
+credits = 0
+
+for line in s.split('\n'):
+    if line != '':
+        if code in line:
+            print(line)
+            credit = line.split(',')[1]
+            credits += int(credit)
+            
+print('Total: ' + str(credits) + ' credits')
