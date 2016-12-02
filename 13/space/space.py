9585dd0d38be6a164eb36f5d6e3816e70721d0f0
psychoAB <dome.2499@hotmail.com>
2016-12-02 14:57:32 +0700

    space: add world class to manage models

--- 13/space/space.py
+++ 13/space/space.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import arcade
-from models import Ship
+from models import World, Ship
 
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
@@ -12,8 +12,9 @@ class SpaceGameWindow(arcade.Window):
 
         arcade.set_background_color(arcade.color.BLACK)
 
-        self.ship = Ship(100, 100)
         self.ship_sprite = arcade.Sprite('images/ship.png')
+
+        self.world = World(width, height)
 
     def on_draw(self):
         arcade.start_render()
@@ -21,10 +22,8 @@ class SpaceGameWindow(arcade.Window):
         self.ship_sprite.draw()
 
     def animate(self, delta):
-        ship = self.ship
-
-        ship.animate(delta)
-        self.ship_sprite.set_position(ship.x, ship.y)
+        self.world.animate(delta)
+        self.ship_sprite.set_position(self.world.ship.x, self.world.ship.y)
 
 if __name__ == '__main__':
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
